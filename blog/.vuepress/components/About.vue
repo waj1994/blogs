bad1447b493d1f3bc2f8fadb284592921a0c39b4
王杰 <wangjie3763@boe.com.cn>
2023-12-14 15:19:15 +0800

    feat(about): 添加背景

---
+++ blog/.vuepress/components/About.vue
@@ -0,0 +1,20 @@
+<!--  -->
+<template>
+  <div class="about">
+    <Content />
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style lang="stylus" scoped>
+.about {
+  min-height: 100%
+  background: url('/bg.png') no-repeat right bottom
+  background-size: 180px
+
+  @media (max-width: $MQMobile) {
+    background-size: 100px
+  }
+}
+</style>
