7f9aa6421ab58bbf6bbb5ced651710845bbddbce
王杰 <wangjie3763@boe.com.cn>
2023-12-14 13:35:14 +0800

    feat(vuepress): title配置修改

--- blog/.vuepress/config.js
+++ blog/.vuepress/config.js
@@ -1,45 +1,45 @@
-const path = require('path')
+const path = require('path');
 
 module.exports = {
   alias: {
-    '@assets': path.resolve(__dirname, '../assets')
+    '@assets': path.resolve(__dirname, '../assets'),
   },
   title: '小王',
   description: 'web开发，前端开发，javascript，react',
   head: [
-    ['meta', { name: 'keyword', content: '前端, javascript, vue, react, git' }]
+    ['meta', { name: 'keyword', content: '前端, javascript, vue, react, git' }],
   ],
   theme: '@vuepress/blog',
   themeConfig: {
     smoothScroll: true,
     nav: [
       { text: '学海无涯', link: '/study/' },
-      { text: '苦作舟', link: '/pit/' }
+      { text: '苦作舟', link: '/pit/' },
     ],
     directories: [
       {
         id: 'study',
         dirname: 'study',
-        title: '学无止境',
+        title: '学海无涯',
         path: '/study/',
-        itemPermalink: '/study/:slug'
+        itemPermalink: '/study/:slug',
       },
       {
         id: 'pit',
         dirname: 'pit',
-        title: '踩过的坑',
+        title: '苦作舟',
         path: '/pit/',
-        itemPermalink: '/pit/:slug'
-      }
+        itemPermalink: '/pit/:slug',
+      },
     ],
 
     footer: {
       copyright: [
         {
           text: 'xw © 2023',
-          link: ''
-        }
-      ]
-    }
-  }
-}
+          link: '',
+        },
+      ],
+    },
+  },
+};
