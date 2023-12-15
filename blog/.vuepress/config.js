1ee5eca48125bc38c2c2ddd844a8c201e40c6e0c
王杰 <wangjie3763@boe.com.cn>
2023-12-15 11:03:15 +0800

    feat(config): 增加百度搜素验证头

--- blog/.vuepress/config.js
+++ blog/.vuepress/config.js
@@ -8,6 +8,7 @@ module.exports = {
   description: 'web开发,前端开发,javascript,react',
   head: [
     ['meta', { name: 'keyword', content: '前端, javascript, vue, react, git' }],
+    ['meta', { name: 'baidu-site-verification', content: 'codeva-bbNA5LdLps' }],
     ['link', { rel: 'icon', href: '/favicon.ico' }],
   ],
   theme: '@vuepress/blog',
