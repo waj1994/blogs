50b52b59a5e45b646e244faf539434df8a76dbb3
王杰 <wangjie3763@boe.com.cn>
2023-12-15 11:35:43 +0800

    feat(config): 添加html头meta

--- blog/.vuepress/config.js
+++ blog/.vuepress/config.js
@@ -9,6 +9,10 @@ module.exports = {
   head: [
     ['meta', { name: 'keyword', content: '前端, javascript, vue, react, git' }],
     ['meta', { name: 'baidu-site-verification', content: 'codeva-bbNA5LdLps' }],
+    [
+      'meta',
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+    ],
     ['link', { rel: 'icon', href: '/favicon.ico' }],
   ],
   theme: '@vuepress/blog',
