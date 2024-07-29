2b4bf881ff792da4dbcd79b38fcfc3a3b6516bac
王杰 <wangjie3763@boe.com.cn>
2024-07-29 09:17:50 +0800

    feat(title): 修改title

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,7 +7,7 @@ import Header from '@/components/Header';
 import './globals.css';
 
 export const metadata: Metadata = {
-  title: `process.env.SITE_TITLE`,
+  title: process.env.SITE_TITLE,
   description:
     '欢迎来到一个跟前端相关的博客。这里有js、vue、react、node等相关的知识点，还有常见的面识题，日常开发中遇到的一些比较难搞的问题等等；我们从基本使用，到底层原理，从方方面面带您领略前端的魅力。让我们一起加油，成为优秀的前端coder。',
   other: {
