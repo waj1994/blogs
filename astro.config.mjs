7863e8fb4a12242ab7e76167cfc662a258b2c476
王杰 <wangjie3763@boe.com.cn>
2024-07-31 09:33:40 +0800

    feat(vercel): 移除web analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,6 @@ import mdx from '@astrojs/mdx'
 import sitemap from '@astrojs/sitemap'
 import UnoCSS from 'unocss/astro'
 import icon from 'astro-icon'
-import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
@@ -17,9 +16,5 @@ export default defineConfig({
       injectReset: true,
     }),
     icon(),
-  ],
-  output: 'server',
-  adapter: vercel({
-    webAnalytics: { enabled: true }
-  }),
+  ]
 })
