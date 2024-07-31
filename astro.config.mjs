1fb0404419c73b2a6b60842fa82d7d464f57be81
王杰 <wangjie3763@boe.com.cn>
2024-07-31 13:36:01 +0800

    feat(giscus): 添加giscus评论

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,14 @@
-import { defineConfig } from 'astro/config'
-import mdx from '@astrojs/mdx'
-import sitemap from '@astrojs/sitemap'
-import UnoCSS from 'unocss/astro'
-import icon from 'astro-icon'
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import UnoCSS from "unocss/astro";
+import icon from "astro-icon";
+import starlightDocSearch from "@astrojs/starlight-docsearch";
+import { SITE_TITLE } from "./src/consts";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://wj1994.cn',
+  site: "https://wj1994.cn",
   integrations: [
     mdx(),
     sitemap({
@@ -16,5 +18,10 @@ export default defineConfig({
       injectReset: true,
     }),
     icon(),
-  ]
-})
+    starlightDocSearch({
+      appId: "YOUR_APP_ID",
+      apiKey: "YOUR_SEARCH_API_KEY",
+      indexName: SITE_TITLE,
+    }),
+  ],
+});
