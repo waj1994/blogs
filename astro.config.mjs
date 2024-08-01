f185ab97cb69baf6b591815076fa531b78d82bfb
王杰 <wangjie3763@boe.com.cn>
2024-08-01 10:45:53 +0800

    feat(algolia): 搜索功能

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,6 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import UnoCSS from "unocss/astro";
 import icon from "astro-icon";
-import starlightDocSearch from "@astrojs/starlight-docsearch";
-import { SITE_TITLE } from "./src/consts";
 
 // https://astro.build/config
 export default defineConfig({
@@ -18,10 +16,5 @@ export default defineConfig({
       injectReset: true,
     }),
     icon(),
-    starlightDocSearch({
-      appId: "YOUR_APP_ID",
-      apiKey: "YOUR_SEARCH_API_KEY",
-      indexName: SITE_TITLE,
-    }),
   ],
 });
